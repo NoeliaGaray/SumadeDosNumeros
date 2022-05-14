259be486f14d8e67f143becab7f2c1032fa9dfed
NoeliaGaray <103612855+NoeliaGaray@users.noreply.github.com>
2022-05-13 21:54:09 -0300

    Eliminé un par de códigos

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ rotulo1.innerHTML = "Ingrese el primer número";
 rotulo2.innerHTML = "Ingrese el segundo número";
 
 let btnEnv = document.getElementById("btnEnviar");
-let primerNumero: number = Number(dato1.value);
-let segundoNumero: number = Number(dato2.value);
-let resultado: number = primerNumero + segundoNumero;
 
 btnEnv.addEventListener("click", () => {
   let primerNumero: number = Number(dato1.value);
@@ -13,5 +10,5 @@ btnEnv.addEventListener("click", () => {
   console.log("el segundo número es:", segundoNumero);
   let resultado: number = primerNumero + segundoNumero;
   console.log("El resultado de la suma es:", resultado);
-  //
+  
 });
